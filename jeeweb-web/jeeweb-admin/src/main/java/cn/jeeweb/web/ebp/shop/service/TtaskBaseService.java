1ed939077e8e1b8b87d8dc056c52d8b033032380
YANGPENG <MhYpYz1988@163.com>
2018-12-17 22:02:17 +0800

    商铺表创建

--- jeeweb-web/jeeweb-admin/src/main/java/cn/jeeweb/web/ebp/shop/service/TtaskBaseService.java
+++
@@ -1,8 +0,0 @@
-package cn.jeeweb.web.ebp.shop.service;
-
-import cn.jeeweb.common.mybatis.mvc.service.ICommonService;
-import cn.jeeweb.web.ebp.shop.entity.TtaskBase;
-
-public interface TtaskBaseService extends ICommonService<TtaskBase> {
-
-}
--- jeeweb-web/jeeweb-admin/src/main/resources/static/shop/global.js
+++
@@ -1,6 +0,0 @@
-//input 框失去焦点后判断基本是否为空等
-function inputDefocus(item){
-    if($(item).val()==''){
-        $(item).parent().parent().addClass('error')
-    }
-}
