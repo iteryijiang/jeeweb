dca2847be1e82ae84b852974b606a550eb3bf7cb
YIJIANG <yijiang331>
2019-01-03 21:36:27 +0800

    门店管理- 增删改查 01.03

--- jeeweb-web/jeeweb-admin/src/main/java/cn/jeeweb/web/ebp/shop/controller/TshopBaseController.java
+++ jeeweb-web/jeeweb-admin/src/main/java/cn/jeeweb/web/ebp/shop/controller/TshopBaseController.java
@@ -1,6 +1,9 @@
 package cn.jeeweb.web.ebp.shop.controller;
 
+import cn.jeeweb.common.http.DuplicateValid;
 import cn.jeeweb.common.http.PageResponse;
+import cn.jeeweb.common.http.Response;
+import cn.jeeweb.common.http.ValidResponse;
 import cn.jeeweb.common.mvc.annotation.ViewPrefix;
 import cn.jeeweb.common.mvc.controller.BaseBeanController;
 import cn.jeeweb.common.mybatis.mvc.wrapper.EntityWrapper;
@@ -15,20 +18,20 @@ import cn.jeeweb.web.aspectj.annotation.Log;
 import cn.jeeweb.web.aspectj.enums.LogType;
 import cn.jeeweb.web.ebp.shop.entity.TshopBase;
 import cn.jeeweb.web.ebp.shop.service.TshopBaseService;
+import cn.jeeweb.web.modules.sys.entity.User;
 import cn.jeeweb.web.utils.UserUtils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 
 @RestController
@@ -77,5 +80,88 @@ public class TshopBaseController extends BaseBeanController<TshopBase> {
         String content = JSON.toJSONString(pagejson, filter);
         StringUtils.printJson(response,content);
     }
+
+
+    @GetMapping(value = "add")
+    public ModelAndView add(Model model, HttpServletRequest request, HttpServletResponse response) {
+        model.addAttribute("data", new TshopBase());
+        // 查询所有的角色List
+        return displayModelAndView ("edit_shop");
+    }
+    @PostMapping("add")
+    @Log(logType = LogType.INSERT)
+    public Response add(TshopBase entity, BindingResult result,
+                        HttpServletRequest request, HttpServletResponse response) {
+        // 验证错误
+        this.checkError(entity,result);
+        entity.setPlatform("jd");
+        entity.setUserid(UserUtils.getPrincipal().getId());
+        tshopBaseService.insert(entity);
+        return Response.ok("添加成功");
+    }
+    @GetMapping(value = "{id}/update")
+    public ModelAndView update(@PathVariable("id") String id, Model model, HttpServletRequest request,
+                               HttpServletResponse response) {
+        TshopBase shopbase = tshopBaseService.selectById(id);
+        model.addAttribute("data", shopbase);
+        return displayModelAndView ("edit_shop");
+    }
+    @PostMapping("{id}/update")
+    @Log(logType = LogType.UPDATE)
+    public Response update(TshopBase entity, BindingResult result,
+                           HttpServletRequest request, HttpServletResponse response) {
+        // 验证错误
+        this.checkError(entity,result);
+        entity.setUserid(UserUtils.getPrincipal().getId());
+        tshopBaseService.insertOrUpdate(entity);
+        return Response.ok("更新成功");
+    }
+
+    @PostMapping("{id}/delete")
+    @Log(logType = LogType.DELETE)
+    @RequiresMethodPermissions("delete")
+    public Response delete(@PathVariable("id") String id) {
+        tshopBaseService.deleteById(id);
+        return Response.ok("删除成功");
+    }
+    /*
+    批量删除
+    */
+    @PostMapping("batch/delete")
+    @Log(logType = LogType.DELETE)
+    @RequiresMethodPermissions("delete")
+    public Response batchDelete(@RequestParam("ids") String[] ids) {
+        List<String> idList = java.util.Arrays.asList(ids);
+        tshopBaseService.deleteBatchIds(idList);
+        return Response.ok("删除成功");
+    }
+
+
+    @PostMapping(value = "validate")
+    public ValidResponse validate(DuplicateValid duplicateValid, HttpServletRequest request) {
+        ValidResponse validResponse = new ValidResponse();
+        Boolean valid = Boolean.FALSE;
+        try {
+            EntityWrapper<TshopBase> entityWrapper = new EntityWrapper<TshopBase>(entityClass);
+            valid = tshopBaseService.doValid(duplicateValid,entityWrapper);
+            if (valid) {
+                validResponse.setStatus("y");
+                validResponse.setInfo("验证通过!");
+            } else {
+                validResponse.setStatus("n");
+                if (!StringUtils.isEmpty(duplicateValid.getErrorMsg())) {
+                    validResponse.setInfo(duplicateValid.getErrorMsg());
+                } else {
+                    validResponse.setInfo("当前信息重复!");
+                }
+            }
+        } catch (Exception e) {
+            validResponse.setStatus("n");
+            validResponse.setInfo("验证异常，请检查字段是否正确!");
+        }
+        return validResponse;
+    }
+
+
 
 }
