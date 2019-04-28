37e3bd105908a11986845723e76e7b2ae86b67c5
YIJIANG <yijiang331>
2019-04-28 15:03:24 +0800

    增加 优惠奍 详细信息 查询方法

--- jeeweb-web/jeeweb-admin/src/main/java/cn/jeeweb/web/ebp/shop/spider/JDUnionApi.java
+++ jeeweb-web/jeeweb-admin/src/main/java/cn/jeeweb/web/ebp/shop/spider/JDUnionApi.java
@@ -1,6 +1,7 @@
 package cn.jeeweb.web.ebp.shop.spider;
 
 import cn.jeeweb.beetl.tags.dict.DictUtils;
+import cn.jeeweb.common.utils.FastJsonUtils;
 import com.jd.open.api.sdk.DefaultJdClient;
 import com.jd.open.api.sdk.JdClient;
 import com.jd.open.api.sdk.JdException;
@@ -8,6 +9,7 @@ import jd.union.open.coupon.importation.request.CouponReq;
 import jd.union.open.coupon.importation.request.UnionOpenCouponImportationRequest;
 import jd.union.open.coupon.importation.response.UnionOpenCouponImportationResponse;
 import jd.union.open.coupon.query.request.UnionOpenCouponQueryRequest;
+import jd.union.open.coupon.query.response.CouponResp;
 import jd.union.open.coupon.query.response.UnionOpenCouponQueryResponse;
 import jd.union.open.promotion.bysubunionid.get.request.UnionOpenPromotionBysubunionidGetRequest;
 import jd.union.open.promotion.bysubunionid.get.response.UnionOpenPromotionBysubunionidGetResponse;
@@ -71,6 +73,14 @@ public class JDUnionApi {
         promotionCodeReq.setCouponUrl(couponurl);
         request.setPromotionCodeReq(promotionCodeReq);
         UnionOpenPromotionBysubunionidGetResponse response = client.execute(request);
+        int code = response.getCode();
+        String message = "";
+        if(code==200){
+            // 200为 生成成功
+        }else{
+            // 失败,返回失败消息
+            message = response.getMessage();
+        }
         String shortURL = response.getData().getShortURL();
         System.out.println("生成的短链接为："+shortURL);
         return shortURL;
@@ -85,7 +95,7 @@ public class JDUnionApi {
         可用数量低于1000个限制发布
         有效期低于生效时间后72小时的  限制发布
      */
-    public String getCouponInfo(String couponurl)throws JdException{
+    public String getCouponInfoByJDAPI(String couponurl)throws JdException{
         String SERVER_URL = "https://router.jd.com/api";
         String appKey = "5e13fa6becc380b136749e57e2d52934";
         String appSecret ="ffaf8f1b12384d9c89b964f08ff29b36";
@@ -94,8 +104,28 @@ public class JDUnionApi {
         JdClient client=new DefaultJdClient(SERVER_URL,accessToken,appKey,appSecret);
 
         UnionOpenCouponQueryRequest request = new UnionOpenCouponQueryRequest();
-        request.setCouponUrls(new String[]{"+couponurl+"});
+        request.setCouponUrls(new String[]{couponurl});
         UnionOpenCouponQueryResponse response = client.execute(request);
+        int code = response.getCode();
+        String message = "";
+        System.out.println("data length::::"+response.getData().length);
+        CouponResp[] datas = response.getData();
+        CouponResp data = datas[0];
+        String yxzt =  data.getYn(); //有效状态
+        long remainNum = data.getRemainNum(); //优惠奍剩余张数
+        long takeBeginTime = data.getTakeBeginTime(); //开始有效时间
+        long takeEndTime = data.getTakeEndTime(); //到期 有效时间
+        double discount = data.getDiscount(); // 奍面额
+        System.out.println("remainNum:"+remainNum);
+        System.out.println("takeBeginTime:"+takeBeginTime);
+        System.out.println("takeEndTime:"+takeEndTime);
+        System.out.println("discount:"+discount);
+        if(code==200){
+            // 200为 生成成功
+        }else{
+            // 失败,返回失败消息
+            message = response.getMessage();
+        }
         System.out.println(response.getData().toString());
         return response.getData().toString();
     }
@@ -106,13 +136,13 @@ public class JDUnionApi {
         JDUnionApi JDUnionApi = new JDUnionApi();
         //JDUnionApi.couponImport();
         try {
-            JDUnionApi.getCouponInfo("http://coupon.m.jd.com/coupons/show.action?key=203d6fb476074ecab137df29da8903ab&roleId=18950122&to=mall.jd.com/index-910797.html");
+            JDUnionApi.getCouponInfoByJDAPI("http://coupon.m.jd.com/coupons/show.action?key=203d6fb476074ecab137df29da8903ab&roleId=18950122&to=mall.jd.com/index-910797.html");
             //41011489021
             //String shorturl = JDUnionApi.getCouponURL("41011489021","http://coupon.m.jd.com/coupons/show.action?key=203d6fb476074ecab137df29da8903ab&roleId=18950122&to=mall.jd.com/index-910797.html");
             // shorturl 不为空  才去生成二维码
             //QRCodeUtil.getInstance().genQrCodeImg(null, 300, 300, "d:\\", "qrcode4.jpg", shorturl);
         }catch (Exception e){
-
+            e.printStackTrace();
         }
 
     }
